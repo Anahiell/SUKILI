bee6791c1099d8067b58bf484de0187819addac2
Бурдейный Евгений <anahielitl@gmail.com>
2023-07-20 19:46:51 +0300

    reload

--- getSortBook.sql
+++ getSortBook.sql
@@ -2,34 +2,49 @@
 --критерии:имя автора,фамилия авт, тематика, категория. Кроме того , список должен быть отсортирован по номеру поля
 --указанному в 5-м параметре, в направлении, указанном 6-м параметре(sp_executesql)
 ALTER PROCEDURE GetSortBook
-    @SortField VARCHAR(100),
-    @SortDirection INT
+    @A_name NVARCHAR(50) = '',
+    @A_Lastname NVARCHAR(50) = '',
+    @Thme NVARCHAR(50) = '',
+    @Categ NVARCHAR(50) = '',
+    @SortField INT = 1,
+    @SortDirection NVARCHAR(4) = 'ASC' 
 AS
 BEGIN
-    DECLARE @OrderBy NVARCHAR(4)
+    IF @SortField < 1 OR @SortField > 5
+    BEGIN
+        PRINT 'wrong sort number'
+        RETURN
+    END
 
-    IF @SortDirection = 1
-        SET @OrderBy = 'ASC'
-    ELSE
-        SET @OrderBy = 'DESC'
+    DECLARE @din NVARCHAR(MAX)
 
-    SELECT A.first_name AS AuthorFirstName, A.last_name AS AuthorLastName, T.name AS Theme, C.name AS Category
-    FROM Book B
-    LEFT JOIN Author A ON B.id_author = A.id
-    LEFT JOIN Theme T ON B.id_theme = T.id
-    LEFT JOIN Category C ON B.id_category = C.id
-    ORDER BY
-        CASE
-            WHEN @SortField = 'AuthorFirstName' THEN A.first_name
-            WHEN @SortField = 'AuthorLastName' THEN A.last_name
-            WHEN @SortField = 'Theme' THEN T.name
-            WHEN @SortField = 'Category' THEN C.name
-        END
-        COLLATE SQL_Latin1_General_CP1_CI_AS,--stackower
-        CASE
-            WHEN @SortDirection = 1 THEN B.id
-        END ASC,
-        CASE
-            WHEN @SortDirection = 2 THEN B.id
-        END DESC
+    SET @din = '
+    SELECT a.first_name AS [Author name], a.last_name AS [Author last name], t.name AS [Theme],
+           c.name AS [Category]
+    FROM Author a
+    JOIN Book b ON a.id = b.id_author
+    JOIN Theme t ON b.id_theme = t.id
+    JOIN Category c ON b.id_category = c.id
+    WHERE a.first_name LIKE ''%'' + @A_name + ''%'' AND
+          a.last_name LIKE ''%'' + ''' + @A_Lastname + ''' AND
+		  t.name LIKE ''%'' + ''' + @Thme + ''' AND
+		  c.name LIKE ''%'' + ''' + @Categ + ''''
+
+    SET @din = @din + '
+    ORDER BY ' +
+    CASE @SortField
+        WHEN 1 THEN 'a.first_name'
+        WHEN 2 THEN 'a.last_name'
+        WHEN 3 THEN 't.name'
+        WHEN 4 THEN 'c.name'
+    END +
+    ' ' + @SortDirection
+
+    --PRINT @din
+    EXEC sp_executesql @din, N'@A_name NVARCHAR(50), @A_Lastname NVARCHAR(50), @Thme NVARCHAR(50), @Categ NVARCHAR(50)',
+                       @A_name, @A_Lastname, @Thme, @Categ
 END
+
+exec GetSortBook
+@A_name = 'Алек',
+@SortDirection='DESC'
